7473697b9a14888e3b43535943be75d5d9c0eecd
benjaminclosson1 <benjaminclosson1@gmail.com>
2020-10-06 10:02:28 -0700

    Commit 1 initial connections in place

---
+++ server.js
@@ -0,0 +1,31 @@
+// ------------------------------------- REQUIREMENTS
+// CORE MODULES
+// THIRD PARTY MODULES
+// CUSTOM MODULES
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.set('view engine', 'ejs');
+
+
+
+
+// ------------------------------------- MIDDLEWARE
+
+
+
+
+
+// ------------------------------------- ROUTES
+
+// Home Route
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+
+
+// ------------------------------------- LISTENER
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
